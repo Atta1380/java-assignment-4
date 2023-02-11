c515ff9dc1900725d5e47c80e34ad816fb032689
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-02-11 21:02:06 +0500

    added a html and javascript files

---
+++ app.js
@@ -0,0 +1,145 @@
+//alert("atta");
+
+
+//Assignment---9-11--
+// ---question--1
+
+//var city = "karachi";
+//var city =prompt("Enter the name of city: ");
+//if(city === "karachi ") {
+  // alert("Welcom to the city of light);"
+//} else {
+//
+  //  alert("Welcome to karachi")
+//}
+
+// ---question--2
+
+//var gender =prompt("Enter your gender  (male/female); ")
+//if (gender ==="male") {
+  //  alert("Good Morning Sir");
+//} else if (gender === "female") {
+  //  alert("Good Morning Ma'am")
+//} else {
+  //  alert("Write....... male/female")
+    
+//}
+
+
+// ---question--3
+
+//var  light= "signal"
+//var signal =prompt("traffic signal color ")
+//if (signal ==="red") {
+ // alert("stop");
+//} else if (signal ==="yellow") {
+ // alert("ready")
+//} else if (signal ==="green") {
+  //alert("go")
+//}  else {
+ //alert("Incorrect ");
+  //}
+
+
+
+
+// ---question--4
+
+//var  fuel= "less than 250ml"
+//var fuel =prompt("fuel in   (litre) ")
+//if (fuel <0.25) {
+//  alert("Please refill the fuel in your car");
+//}  else {
+//  alert("no fuel ");
+//   }
+
+
+
+// ---question--5
+
+
+//----a.
+
+//var a = 4;
+//if (++a === 5){
+//alert("given condition for variable a is true");
+//}
+
+//----b.
+//var b = 82;
+//if (b++ === 83){
+//alert("given condition for variable b is true");
+//}
+
+//----c. 
+//var c = 12;
+//if (c++ === 13){
+//alert("condition 1 is true");
+//}
+//if (c === 13){
+//alert("condition 2 is true");
+//}
+//if (++c < 14){
+//alert("condition 3 is true");
+//}
+//if(c === 14){
+//alert("condition 4 is true");
+//}
+
+//-----d.
+ //var materialCost = 20000;
+//var laborCost = 2000;
+//var totalCost = materialCost + laborCost;
+//if (totalCost === laborCost + materialCost){
+//alert("The cost equals");
+//}
+//----e. 
+//if (true){
+//alert("True");
+//}
+//if (false){
+//alert("False");
+//}
+
+//---f.
+ //if("car" < "cat"){
+  //  alert("car is smaller than cat");
+ //   }
+
+//----------question--6. 
+
+
+//----------question--7. 
+
+//----------question--8. 
+//var  whether= "given number"
+//var whether =prompt("enter a   (number) ")
+//if (whether % 3 === 0) {
+//  alert(whether +" " + "divisible by 3");
+//} else{
+//  alert(whether + " "+"not divisible by 3");
+//} 
+
+
+//----------question--9.
+
+//var  number= "enter a num"
+//var number =prompt("(enter a number) ")
+//if (number % 2 ===0) {
+//  alert(number + " " + "an even number");
+//}  else {
+//  alert(number + " " + "an odd number");
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
